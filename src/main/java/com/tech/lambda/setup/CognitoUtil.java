213499bd7d3a41a065087534357e8310c968a6ef
Lucas <lucas.santiago@sgroup.com.br>
2024-09-13 09:12:57 -0300

    Ajustando endpoint de login

--- src/main/java/com/tech/lambda/setup/CognitoUtil.java
+++ src/main/java/com/tech/lambda/setup/CognitoUtil.java
@@ -63,6 +63,6 @@ public abstract class CognitoUtil {
 
         AdminInitiateAuthResponse  authResponse = cognitoClient.adminInitiateAuth(authRequest);
 
-        return authResponse.toString();
+        return authResponse.authenticationResult().refreshToken();
     }
 }
